ea348014b1fba436702c1e71968da53c3c760acb
pktatari <tawhidalmuhaimin@gmail.com>
2023-12-26 23:33:24 +0600

    basic layout done

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "../api";
+import TaskCard from "./TaskCard";
 
 const Homepage = () => {
 
@@ -12,10 +13,76 @@ const Homepage = () => {
 
     if (error) return 'An error has occurred: ' + error.message
 
-    console.log(tasks.tasks);
+    // console.log(tasks.tasks);
+    const tasks1 = {
+        "count": 4,
+        "tasks": [
+            {
+                "id": 1,
+                "task": "Design Kanban",
+                "assigned_to": "Robert",
+                "assignee": "Vitasoft",
+                "priority": "Completed",
+                "due_date": "2023-12-27T10:06:15+06:00",
+                "completed": true,
+                "completed_at": "2023-12-26T12:05:16.688403+06:00",
+                "created_at": "2023-12-25T10:06:30+06:00"
+            },
+            {
+                "id": 2,
+                "task": "string",
+                "assigned_to": "string",
+                "assignee": "string",
+                "priority": "string",
+                "due_date": "2019-08-24T20:15:22+06:00",
+                "completed": false,
+                "completed_at": null,
+                "created_at": "2019-08-24T20:15:22+06:00"
+            },
+            {
+                "id": 3,
+                "task": "React APP",
+                "assigned_to": "Lorel",
+                "assignee": "Mandin",
+                "priority": "High",
+                "due_date": "2023-12-26T18:30:00+06:00",
+                "completed": true,
+                "completed_at": "2023-12-26T18:31:00+06:00",
+                "created_at": "2023-12-26T18:31:00+06:00"
+            },
+            {
+                "id": 4,
+                "task": "hellow",
+                "assigned_to": "me",
+                "assignee": "you",
+                "priority": "low",
+                "due_date": "2023-12-06T19:28:00+06:00",
+                "completed": false,
+                "completed_at": "2023-12-15T19:28:00+06:00",
+                "created_at": "2023-12-26T19:28:00+06:00"
+            }
+        ]
+    }
     return (
         <div className="bg-[#F3F4F6] min-h-screen">
-
+            <div className="grid grid-cols-4">
+                <div className="bg-[#082F49] mt-8 mx-4 rounded-lg min-h-[720px]">
+                    <h1 className="text-white text-2xl pl-6 pt-4">Backlog</h1>
+                    {tasks?.tasks?.map((task) => <TaskCard key={task.id} task={task}></TaskCard>)}
+                </div>
+                <div className="bg-[#082F49] mt-8 mx-4 rounded-lg min-h-[720px]">
+                    <h1 className="text-white text-2xl pl-6 pt-4">ToDo</h1>
+                    {tasks?.tasks?.map((task) => <TaskCard key={task.id} task={task}></TaskCard>)}
+                </div>
+                <div className="bg-[#082F49] mt-8 mx-4 rounded-lg min-h-[720px]">
+                    <h1 className="text-white text-2xl pl-6 pt-4">Doing</h1>
+                    {tasks?.tasks?.map((task) => <TaskCard key={task.id} task={task}></TaskCard>)}
+                </div>
+                <div className="bg-[#082F49] mt-8 mx-4 rounded-lg min-h-[720px]">
+                    <h1 className="text-white text-2xl pl-6 pt-4">Done</h1>
+                    {tasks?.tasks?.map((task) => <TaskCard key={task.id} task={task}></TaskCard>)}
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+
+const TaskCard = ({ task }) => {
+
+    const formatDate = (isoDateString) => {
+        const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+        return new Date(isoDateString).toLocaleString('en-US', options);
+    };
+
+
+    return (
+        <div className='bg-white m-6 p-2 rounded-md'>
+            <div className='flex justify-between items-center pb-5'>
+                <div>
+                    <h1 className='text-2xl'>{task.task}</h1>
+                    <p className='text-gray-700 text-xs'>Created at: {formatDate(task.created_at)}</p>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    {task?.priority === 'high' && <p className='bg-red-500 w-fit px-1 text-xs rounded-lg py-1 text-white' >High priority</p>}
+                    {task?.priority === 'moderate' && <p className='bg-[#FFAA33] w-fit px-1 text-xs rounded-lg py-1 text-white' >Medium priority</p>}
+                    {task?.priority === 'medium' && <p className='bg-[#FFAA33] w-fit px-1 text-xs rounded-lg py-1 text-white' >Medium priority</p>}
+                    {task?.priority === 'low' && <p className='bg-[#097969] w-fit px-1 text-xs rounded-lg py-1 text-white' >Low priority</p>}
+                </div>
+            </div>
+            <p className='text-gray-700 text-sm'>Assigned to <span className='font-bold'>{task.assigned_to}</span> by <span className='font-bold'>{task.assignee}</span></p>
+            <p className='text-gray-700 text-sm'> Due Date <span className='font-bold'>{formatDate(task.due_date)}</span></p>
+            {task.completed && <p className='bg-[#097969] w-fit px-1 text-xs rounded-md py-1 mt-4 text-white'>Completed at: {task.completed_at}</p>}
+        </div>
+    );
+};
+
+TaskCard.propTypes = {
+    task: PropTypes.object,
+    refetch: PropTypes.func,
+};
+
+export default TaskCard;
